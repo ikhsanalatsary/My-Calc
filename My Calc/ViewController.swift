07b8db909e74658cd1e6bf674490482c8ffb4a6e
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-08-15 13:36:36 +0700

    Initial Commit

---
+++ My Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Calc
+//
+//  Created by Gabriel Lai on 15/8/18.
+//  Copyright © 2018 Abdul Fattah Ikhsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
